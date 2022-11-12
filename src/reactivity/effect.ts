4555ebe8ca1745d9c06d292754458e096ba3129d
recoveryMonster <fengsongting@gmail.com>
2022-11-13 03:31:25 +0800

    feat: stop 功能优化

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,87 +1,108 @@
-import { extend } from "./shared/utils"
-
+import { extend } from "./shared/utils";
+// 当前的副作用函数
+let activeEffect;
+// 是否应该收集依赖
+let shouldTrack;
 class ReactiveEffect {
-  private _fn
-  public deps = []
-  private _isActive = true
-  public onStop?: () => {}
+  private _fn;
+  public deps = [];
+  public isActive = true;
+  public onStop?: () => {};
   constructor(fn, public scheduler?) {
-    this._fn = fn
+    this._fn = fn;
   }
   // 运行副作用函数
   run() {
+    // 如果调用 stop 函数之后 手动调用 runner 函数
+    if (!this.isActive) {
+      // 重置 isActive 和 shouldTrack 状态，执行 runner 函数之后 重新访问属性可以收集依赖
+      this.isActive = true;
+      shouldTrack = true;
+      return this._fn();
+    }
+    // 否则说明当前是可收集的状态
+    shouldTrack = true;
+
     activeEffect = this;
     // 获取调用函数的返回值
-    const res = this._fn()
-    return res
+    const res = this._fn();
+    // 重制可收集状态
+    shouldTrack = false;
+    return res;
   }
 
   stop() {
     // 调用 stop 方法 cleanupEffect 只执行一次
-    if (this._isActive) {
+    if (this.isActive) {
       // 如果 onStop 存在 则会调用一次 onStop
       if (this.onStop) {
-        this.onStop()
+        this.onStop();
       }
       cleanupEffect(this);
-      this._isActive = false
+      this.isActive = false;
     }
   }
 }
 // 从收集了 effect 的 deps 中删除 effect
 function cleanupEffect(effect) {
-  effect.deps.forEach(deps => {
-    deps.delete(effect)
-  })
+  effect.deps.forEach((deps) => {
+    deps.delete(effect);
+  });
+  // 由于当前 effect 的 deps 已经清空 所以可以直接清空
+  effect.deps.length = 0;
 }
-const targetMap = new WeakMap()
+const targetMap = new WeakMap();
 // 收集依赖
 // WeakMap -> Map -> Set
 export function track(target, key) {
-  // 如果没有副作用函数 说明无需依赖需要收集
-  if (!activeEffect) return
+  // 如果没有副作用函数或者不是收集的状态 说明无需依赖需要收集
+  if (!isTracking()) return;
   let depsMap = targetMap.get(target);
   if (!depsMap) {
-    targetMap.set(target, (depsMap = new Map()))
+    targetMap.set(target, (depsMap = new Map()));
   }
   let deps = depsMap.get(key);
   if (!deps) {
     depsMap.set(key, (deps = new Set()));
   }
-  deps.add(activeEffect)
+  deps.add(activeEffect);
   // 将 deps 添加到 activeEffect.deps 用于 stop 函数时将依赖删除
-  activeEffect.deps.push(deps)
+  activeEffect.deps.push(deps);
 }
+
+// 是否是正在收集依赖
+function isTracking() {
+  return activeEffect !== undefined && shouldTrack;
+}
+
 // 触发更新
 export function trigger(target, key) {
-  const depsMap = targetMap.get(target)
-  if (!depsMap) return
-  const deps = depsMap.get(key)
+  const depsMap = targetMap.get(target);
+  if (!depsMap) return;
+  const deps = depsMap.get(key);
   // 获取 key 所对应的依赖执行
   deps.forEach((effect) => {
     if (effect.scheduler) {
-      effect.scheduler()
+      effect.scheduler();
     } else {
       effect.run();
     }
   });
 }
 
-// 当前的副作用函数
-let activeEffect
 export function effect(fn, options: any = {}) {
-  const _effect = new ReactiveEffect(fn, options.scheduler)
-  extend(_effect, options)
+  const _effect = new ReactiveEffect(fn, options.scheduler);
+  extend(_effect, options);
   // 执行副作用函数 其中访问对象时会进行依赖收集
-  _effect.run()
+  _effect.run();
   // 返回 runner 函数
-  const runner: any = _effect.run.bind(_effect)
-  runner.effect = _effect
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
 
-  return runner
+  return runner;
 }
 
 // 调用 stop 的时候应该将 effect 从 deps 中删除
 export function stop(runner) {
-  runner.effect.stop()
+  runner.effect.stop();
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,31 +1,31 @@
 import { reactive } from "../reactive";
 import { effect, stop } from "../effect";
 
-describe('reactivity/effect', () => {
-  it('should handle single effect', () => {
+describe("reactivity/effect", () => {
+  it("should handle single effect", () => {
     const user = reactive({
       age: 18,
-    })
-    let userAge
-    effect(() => userAge = user.age)
-    expect(userAge).toBe(18)
-    user.age++
-    expect(userAge).toBe(19)
+    });
+    let userAge;
+    effect(() => (userAge = user.age));
+    expect(userAge).toBe(18);
+    user.age++;
+    expect(userAge).toBe(19);
   });
 
-  it('should ruturn runner when effect was called', () => {
+  it("should ruturn runner when effect was called", () => {
     let count = 10;
     const runner = effect(() => {
-      count++
-      return 'count'
-    })
-    expect(count).toBe(11)
-    const res = runner()
-    expect(count).toBe(12)
-    expect(res).toBe('count')
+      count++;
+      return "count";
+    });
+    expect(count).toBe(11);
+    const res = runner();
+    expect(count).toBe(12);
+    expect(res).toBe("count");
   });
 
-  it('schedluer', () => {
+  it("schedluer", () => {
     // 1、通过 effect 的第二个参数指定名为 scheduler 的 fn
     // 2、effect 第一次调用时，fn 仍然会调用
     // 3、响应式数据 set 时，update 触发不会调用 fn，而是 schdeuer 执行
@@ -56,28 +56,33 @@ describe('reactivity/effect', () => {
     expect(dummy).toBe(2);
   });
 
-  it('stop', () => {
+  it("stop", () => {
     let dummy;
     const obj = reactive({ prop: 1 });
     const runner = effect(() => (dummy = obj.prop));
     obj.prop = 2;
     expect(dummy).toBe(2);
     stop(runner);
-    obj.prop = 3;
+    // 当执行 stop 之后，设置 obj 的属性 响应式应该消失
+    // obj.prop = 3;
+    obj.prop++;
     expect(dummy).toBe(2);
 
     // stopped effect should still be manually callable
     runner();
     expect(dummy).toBe(3);
+    // 手动执行 runner 之后，再次访问属性 应该是响应式的
+    obj.prop++;
+    expect(dummy).toBe(4);
   });
 });
 
-  it("events: onStop", () => {
-    const onStop = jest.fn();
-    const runner = effect(() => {}, {
-      onStop,
-    });
-    // 执行 stop 函数 onStop 会执行一次
-    stop(runner);
-    expect(onStop).toHaveBeenCalled();
+it("events: onStop", () => {
+  const onStop = jest.fn();
+  const runner = effect(() => {}, {
+    onStop,
   });
+  // 执行 stop 函数 onStop 会执行一次
+  stop(runner);
+  expect(onStop).toHaveBeenCalled();
+});
