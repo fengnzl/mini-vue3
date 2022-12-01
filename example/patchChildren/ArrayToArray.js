d1a1d5f3670a0317960000b175865d80bcd28b01
recoveryMonster <fengsongting@gmail.com>
2022-12-02 00:23:50 +0800

    feat: 更新 element 的children 通过获取最长递增子序列进行相应的对比优化

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -88,21 +88,87 @@ import { h, ref } from "../../lib/guide-mini-vue.esm.js";
 
 // 5.1.1 ab(ced)fg -> ab(ec)fg
 // 中间部分 老的比新的多  那么多出来的可以直接删除（优化删除逻辑）
+// const prevChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "C", id: "c-prev" }, "C"),
+//   h("div", { key: "E" }, "E"),
+//   h("div", { key: "D" }, "D"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ];
+// const nextChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "E" }, "E"),
+//   h("div", { key: "C", id: "c-next" }, "C"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ];
+
+// 移动（节点在新和老的里面，但是位置变了）
+// ab(cde)fg
+// ab(ecd)fg
+// 最长子序列 [1,2]
+// const prevChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "C", id: "c-prev" }, "C"),
+//   h("div", { key: "D" }, "D"),
+//   h("div", { key: "E" }, "E"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ];
+// const nextChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "E" }, "E"),
+//   h("div", { key: "C", id: "c-next" }, "C"),
+//   h("div", { key: "D" }, "D"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ];
+
+// 创建新的节点
+// ab(ce)fg
+// ab(ecd)fg
+// const prevChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "C", id: "c-prev" }, "C"),
+//   h("div", { key: "E" }, "E"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ];
+// const nextChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "E" }, "E"),
+//   h("div", { key: "C", id: "c-next" }, "C"),
+//   h("div", { key: "D" }, "D"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ];
+
+// 综合例子
+// ab(cdez)fg
+// ab(dcye)fg
 const prevChildren = [
   h("div", { key: "A" }, "A"),
   h("div", { key: "B" }, "B"),
   h("div", { key: "C", id: "c-prev" }, "C"),
-  h("div", { key: "E" }, "E"),
   h("div", { key: "D" }, "D"),
-  h("div", { key: "F" }, "F"),
+  h("div", { key: "E" }, "E"),
+  h("div", { key: "Z" }, "Z"),
   h("div", { key: "G" }, "G"),
 ];
 const nextChildren = [
   h("div", { key: "A" }, "A"),
   h("div", { key: "B" }, "B"),
-  h("div", { key: "E" }, "E"),
+  h("div", { key: "D" }, "D"),
   h("div", { key: "C", id: "c-next" }, "C"),
-  h("div", { key: "F" }, "F"),
+  h("div", { key: "Y" }, "Y"),
+  h("div", { key: "E" }, "E"),
   h("div", { key: "G" }, "G"),
 ];
 
